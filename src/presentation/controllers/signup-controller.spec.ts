5357a33228b72d95df803c37af2925c3c4bf7b4a
Rafael Affonso <hrafaelaf@gmail.com>
2021-02-13 22:50:02 -0300

    refactor: move duplicated code to a helper file

--- src/presentation/controllers/signup-controller.spec.ts
+++ src/presentation/controllers/signup-controller.spec.ts
@@ -4,9 +4,11 @@ describe('SignUp Controller', () => {
   test('should return 400 if no name is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
-      email: 'anyemail@email.com',
-      password: 'password',
-      passwordConfirmation: 'password'
+      body: {
+        email: 'anyemail@email.com',
+        password: 'password',
+        passwordConfirmation: 'password'
+      }
     }
 
     const httpResponse = sut.handle(httpRequest)
