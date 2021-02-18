8c1db3050d0ad7e21236f274baa176bb5efc47ba
Rafael Affonso <hrafaelaf@gmail.com>
2021-02-18 08:13:30 -0300

    test: ensure CreateAccountRepository is called with correct values

--- src/data/use-cases/db-create-account/db-create-account.spec.ts
+++ src/data/use-cases/db-create-account/db-create-account.spec.ts
@@ -1,12 +1,10 @@
+import { CreateAccountModel } from '../../../domain/usecases/create-account'
+import { AccountModel } from '../../../domain/usecases/models/account'
 import { DBCreateAccount } from './db-create-account'
+import { CreateAccountRepository } from './protocols/create-account-repository'
 import { Encrypter } from './protocols/encrypter'
 
 describe('DBCreateAccount Usecase', () => {
-  interface SutTypes {
-    sut: DBCreateAccount
-    encrypterStub: Encrypter
-  }
-
   const makeEncrypter = (): Encrypter => {
     class EncrypterStub implements Encrypter {
       async encrypt(value: string): Promise<string> {
@@ -16,11 +14,34 @@ describe('DBCreateAccount Usecase', () => {
     return new EncrypterStub()
   }
 
+  const makeCreateAccountRepositorySub = (): CreateAccountRepository => {
+    class CreateAccountRepositoryStub implements CreateAccountRepository {
+      async execute(data: CreateAccountModel): Promise<AccountModel> {
+        const fakeAccount = {
+          id: 'valid id',
+          name: 'valid name',
+          email: 'validemail@email.com',
+          password: 'hashed_value'
+        }
+        return await new Promise(resolve => resolve(fakeAccount))
+      }
+    }
+    return new CreateAccountRepositoryStub()
+  }
+
+  interface SutTypes {
+    sut: DBCreateAccount
+    encrypterStub: Encrypter
+    createAccountRepositoryStub: CreateAccountRepository
+  }
+
   const makeSut = (): SutTypes => {
     const encrypterStub = makeEncrypter()
+    const createAccountRepositoryStub = makeCreateAccountRepositorySub()
     return {
-      sut: new DBCreateAccount(encrypterStub),
-      encrypterStub
+      sut: new DBCreateAccount(encrypterStub, createAccountRepositoryStub),
+      encrypterStub,
+      createAccountRepositoryStub
     }
   }
 
@@ -46,5 +67,17 @@ describe('DBCreateAccount Usecase', () => {
     }
     const promisse = sut.execute(accountData)
     await expect(promisse).rejects.toThrow()
+  })
+
+  test('should call CreateAccountRepository with correct values', async () => {
+    const { sut, createAccountRepositoryStub } = makeSut()
+    const spy = jest.spyOn(createAccountRepositoryStub, 'execute')
+    const accountData = {
+      name: 'valid name',
+      email: 'valid_email@email.com',
+      password: 'valid_password'
+    }
+    await sut.execute(accountData)
+    expect(spy).toHaveBeenLastCalledWith({ ...accountData, password: 'hashed_value' })
   })
 })
