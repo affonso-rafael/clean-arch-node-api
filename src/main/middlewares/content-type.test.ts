43fcea7eb3e4d4749aea3cd037cf2e58def1a1b8
Rafael Affonso <hrafaelaf@gmail.com>
2021-02-19 10:31:01 -0300

    feat: ensure signup route returns success

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -3,21 +3,21 @@ import request from 'supertest'
 
 describe('Content types middleware', () => {
   test('should return default content-type as json', async () => {
-    app.get('/test-content-type/json', (req, res) => {
+    app.get('/test-content-type-json', (req, res) => {
       res.send()
     })
     await request(app)
-      .get('/test-content-type/json')
+      .get('/test-content-type-json')
       .expect('content-type', /json/)
   })
 
   test('should return xml when forced', async () => {
-    app.get('/test-content-type/xml', (req, res) => {
+    app.get('/test-content-type-xml', (req, res) => {
       res.type('xml')
       res.send()
     })
     await request(app)
-      .get('/test-content-type/xml')
+      .get('/test-content-type-xml')
       .expect('content-type', /xml/)
   })
 })
